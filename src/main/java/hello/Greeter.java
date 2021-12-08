d369f3518d5b8d161db980c274abe4b6ca65af38
Tr1ckC0 <trick.co@yahoo.com>
2021-12-07 21:31:00 -0600

    build.gradle

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,4 +1,7 @@
 package hello;
 
 public class Greeter {
+    public String sayHello() {
+        return "Hello World!";
+    }
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,7 @@ package hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
     }
 }
